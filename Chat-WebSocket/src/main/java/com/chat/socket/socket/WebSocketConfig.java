f6cf1e03aaa1ee4f5260888b8db6e9fd0174410b
iahimnafets <80846557+iahimnafets@users.noreply.github.com>
2022-06-26 15:05:19 +0200

    Update WebSocketConfig.java

--- Chat-WebSocket/src/main/java/com/chat/socket/socket/WebSocketConfig.java
+++ Chat-WebSocket/src/main/java/com/chat/socket/socket/WebSocketConfig.java
@@ -40,28 +40,5 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
         */
     }
 
-    /*
-    @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
 
-    @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-        logger.info("Received a new web socket connection");
-    }
-
-    @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-        if(username != null) {
-            logger.info("User Disconnected : " + username);
-            Message chatMessage = new Message();
-            //     chatMessage.setType(ChatMessage.MessageType.LEAVE);
-            //     chatMessage.setSender(username);
-            messagingTemplate.convertAndSend("/topic/public", chatMessage);
-        }
-    }
-
-     */
 }
